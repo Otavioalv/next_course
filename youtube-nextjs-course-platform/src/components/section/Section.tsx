ee95f53a0df893f0dcd5783c18a8c6b2100c66f4
gerson <gersonalmeida399@gmail.com>
2026-03-13 15:35:14 -0400

    style: scrollbar modificado

--- youtube-nextjs-course-platform/src/components/section/Section.tsx
+++ youtube-nextjs-course-platform/src/components/section/Section.tsx
@@ -2,7 +2,7 @@
 
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { Card, ICardProps } from "../card/Card";
-import { useRef } from "react";
+import { UIEvent, useEffect, useRef, useState } from "react";
 
 
 interface ISectionProps {
@@ -14,12 +14,28 @@ interface ISectionProps {
 export const Section = ({title, items, variant="grid"}: ISectionProps) => {
     const scrollRef = useRef<HTMLUListElement>(null);
 
+    const [scrollAt, setScrollAt] = useState<'start' | 'middle' | 'end'>('start');
+
 
     const handleScroll = (scroll: number) => {
         scrollRef.current?.scrollTo({
             left: scrollRef.current.scrollLeft + scroll,
             behavior: "smooth",
         });
+    };
+
+    // useEffect(() => {
+        
+    // }, []);
+
+    const handleScrollEvent = (event: UIEvent<HTMLUListElement>) => {
+        if(event.currentTarget.scrollLeft === 0) {
+            setScrollAt('start');
+        }else if((event.currentTarget.scrollWidth - event.currentTarget.clientWidth) === event.currentTarget.scrollLeft) {
+            setScrollAt('end');
+        }else {
+            setScrollAt('middle');
+        }
     };
 
     return (
@@ -34,15 +50,21 @@ export const Section = ({title, items, variant="grid"}: ISectionProps) => {
 
             <ul 
                 ref={scrollRef}
+                onScroll={handleScrollEvent}
                 data-variant={variant}
                 className="grid grid-cols-1 gap-4 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto"
             >
-                <button 
-                    onClick={() => handleScroll(-300)}
-                    className="w-14 h-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14"
-                >
-                    <MdKeyboardArrowLeft size={32}/>
-                </button>
+
+                {variant === "h-list" && (
+                    <button 
+                        disabled={scrollAt === 'start'}
+                        onClick={() => handleScroll(-300)}
+                        className="w-14 h-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14 disabled:opacity-0 transition-opacity active:opacity-80"
+                    >
+                        <MdKeyboardArrowLeft size={32}/>
+                    </button>
+                )}
+
                 {items.map((item) => (
                     <li data-variant={variant} className="w-full data-[variant=h-list]:sm:w-72" key={item.title}>
                         <Card
@@ -53,12 +75,16 @@ export const Section = ({title, items, variant="grid"}: ISectionProps) => {
                         />
                     </li>
                 ))}
-                <button 
-                    onClick={() => handleScroll(300)}
-                    className="w-14 h-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14"
-                >
-                    <MdKeyboardArrowRight size={32}/>
-                </button>
+
+                {variant === "h-list" && (
+                    <button 
+                        disabled={scrollAt === 'end'}
+                        onClick={() => handleScroll(300)}
+                        className="w-14 h-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14 disabled:opacity-0 transition-opacity active:opacity-80"
+                    >
+                        <MdKeyboardArrowRight size={32}/>
+                    </button>
+                )}
             </ul>
         </section>
     );
